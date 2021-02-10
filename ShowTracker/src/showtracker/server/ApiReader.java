97fd56a6240ef856d4a65374c0a0d2a68f33d198
Adel Sabanovic <62741023+Adel-Sabanovic@users.noreply.github.com>
2021-02-10 23:30:57 +0100

    Api
    Fixat med API:et

--- ShowTracker/src/showtracker/server/ApiReader.java
+++ ShowTracker/src/showtracker/server/ApiReader.java
@@ -1,11 +1,45 @@
 package showtracker.server;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class ApiReader {
-    String url = "http://www.omdbapi.com/?t=";
-    String apiKey = "&apikey=be9629f";
-    String json = "?format=JSON";
-    String serie = "friends";
-    public void connectToApi(){
-        System.out.println(url+serie+apiKey);
+
+    private String url = "http://www.omdbapi.com/?t=";
+    private String apiKey = "&apikey=be9629f";
+    private String json = "&?format=JSON";
+    private String serie = "friends";
+    private String season = "&season=";
+    private String episode = "&Episode=";
+
+    private JTextField nameOfSerieTxf;
+
+    private Scanner s1 = new Scanner(System.in);
+    private Scanner s2 = new Scanner(System.in);
+    private Scanner s3 = new Scanner(System.in);
+
+    public void seasonsAndEpisodes(){
+        System.out.println("Mata in en serie eller film i konsolen! ");
+
+        System.out.println("Mata in säsong: ");
+
+        System.out.println("Mata in avsnitt: ");
+
+        System.out.println(url+s1.nextLine()+season+s2.nextInt()+episode+s3.nextInt()+json+apiKey);
+    }
+
+    public void getSeasons(){
+        System.out.println("Mata in en serie eller film i konsolen! ");
+
+        System.out.println("Mata in säsong: ");
+
+        System.out.println(url+s1.nextLine()+season+s2.nextInt()+json+apiKey);
+    }
+
+    public static void main(String[] args) {
+        ApiReader api = new ApiReader();
+       // api.seasonsAndEpisodes();
+        api.getSeasons();
+
     }
 }
