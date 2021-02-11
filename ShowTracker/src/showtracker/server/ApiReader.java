08964a2c8591e967397fbd6f088f096bf66602ff
Adel Sabanovic <62741023+Adel-Sabanovic@users.noreply.github.com>
2021-02-11 16:55:40 +0100

    zcs
    ac

--- ShowTracker/src/showtracker/server/ApiReader.java
+++ ShowTracker/src/showtracker/server/ApiReader.java
@@ -1,10 +1,14 @@
 package showtracker.server;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Scanner;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ApiReader {
-
+    private static HttpURLConnection connection;
     private String url = "http://www.omdbapi.com/?t=";
     private String apiKey = "&apikey=be9629f";
     private String json = "&?format=JSON";
@@ -35,10 +39,20 @@ public class ApiReader {
         System.out.println(url+s1.nextLine()+season+s2.nextInt()+json+apiKey);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ApiReader api = new ApiReader();
        // api.seasonsAndEpisodes();
         api.getSeasons();
+       // URL url = new URL(api.url+api.s1.nextLine()+api.season+api.s2.nextInt()+api.json+api.apiKey);
+        URL url = new URL("http://www.omdbapi.com/?t=&apikey=be9629f");
+        connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+
+        int status = connection.getResponseCode();
+        System.out.println(status);
 
     }
 }
