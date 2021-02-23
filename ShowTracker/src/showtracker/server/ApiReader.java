4422cd6ccb0c17e9b8361ed4cce7bcb75955fe56
robi874 <62009463+robi874@users.noreply.github.com>
2021-02-23 16:52:58 +0100

    Inspected the code, and made some examples using HttpRequest and HttpResponse, made changes in DatabaseReader.java to try out the new OMDbAPI

--- ShowTracker/src/showtracker/server/ApiReader.java
+++ ShowTracker/src/showtracker/server/ApiReader.java
@@ -1,8 +1,18 @@
 package showtracker.server;
 
+import com.mysql.cj.xdevapi.JsonParser;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import javax.swing.*;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -45,11 +55,35 @@ public class ApiReader {
         //api.getSeasons();
         System.out.println("Mata först in en serie/film och tryck Enter! \nMata sedan in säsongen du vill ha info om! ");
         System.out.println("(Säsongen kan endast skrivas som en siffra)");
-        URL url = new URL(api.url+api.s1.nextLine()+api.season+api.s2.nextInt()+api.json+api.apiKey);
-        System.out.println(url);
+        //URL url = new URL(api.url+api.s1.nextLine()+api.season+api.s2.nextInt()+api.json+api.apiKey);
+        //System.out.println(url);
        // URL url = new URL("http://www.omdbapi.com/?t=&apikey=be9629f");
-        connection = (HttpURLConnection) url.openConnection();
+        //connection = (HttpURLConnection) url.openConnection();
+        //System.out.println(api.url+api.s1.nextLine()+api.season+api.s2.nextInt()+api.json+api.apiKey);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(api.url+api.s1.nextLine()+api.season+api.s2.nextInt()+api.json+api.apiKey))
+                .header("Accept", "application/json")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request,HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+            if(response.statusCode() == 200){
 
+                System.out.println(response.body());
+                JSONParser parser = new JSONParser();
+                JSONObject obj = (JSONObject) parser.parse(response.body());
+                System.out.println(obj.get("Title"));
+
+                JSONArray array = (JSONArray) obj.get("Episodes");
+                for (int i = 0; i < array.size(); i++) {
+                    System.out.println(array.get(i));
+                }
+            }
+        } catch (InterruptedException | ParseException e) {
+            e.printStackTrace();
+        }
+        /*
         connection.setRequestMethod("GET");
         connection.setConnectTimeout(5000);
         connection.setReadTimeout(5000);
@@ -59,6 +93,8 @@ public class ApiReader {
         if (status<299){
             System.out.println(" success!");
         }
+
+         */
 
     }
 }
--- ShowTracker/src/showtracker/server/DatabaseReader.java
+++ ShowTracker/src/showtracker/server/DatabaseReader.java
@@ -13,11 +13,15 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.apache.http.HttpResponse;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import showtracker.Episode;
 import showtracker.Helper;
 import showtracker.Show;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpRequest;
 import java.sql.*;
 
 /**
@@ -221,33 +225,116 @@ public class DatabaseReader {
         return ret;
     }
 
+
+    /**
+     * Method to search the API for a show with the provided input.
+     *
+     * @param searchTerms String that contains the search term
+     * @return a double string array that contains shows or null if no shows are found
+     */
     public String[][] searchTheTVDBShows(String searchTerms) {
         String[] arSearchTerms = searchTerms.split(" ");
         StringBuilder sbSearchTerms = new StringBuilder(arSearchTerms[0]);
         for (int i = 1; i < arSearchTerms.length; i++)
             sbSearchTerms.append("%20").append(arSearchTerms[i]);
 
+        //Building request for api
+        String apiKey = "be9629f";
+        HttpRequest req = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.omdbapi.com/?s="+sbSearchTerms.toString()+"&type=series&apikey="+apiKey+"&format="+"json"))
+                .header("Accept", "application/json")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        try {
+            //Respone from API
+            java.net.http.HttpResponse<String> response = java.net.http.HttpClient.newHttpClient().send(req, java.net.http.HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
+            //if response is ok (200)
+            if(response.statusCode() == 200){
+                //if response has error message
+                if(response.body().contains("Too many results.")){
+                    return null;
+                }
+                if(response.body().contains("Series not found!")){
+                    return null;
+                }
+                //otherwise it parses the show and returns the double array
+                else {
+
+                    JSONParser parser = new JSONParser();
+                    JSONObject obj = (JSONObject) parser.parse(response.body());
+                    JSONArray array = (JSONArray) obj.get("Search");
+                    String[][] shows = new String[array.size()][2];
+                    for (int i = 0; i < array.size(); i++) {
+                        JSONObject arrObj = (JSONObject) array.get(i);
+                        shows[i][0] = (String) arrObj.get("Title") + " ("+ arrObj.get("Year")+")";
+                        shows[i][1] = (String) arrObj.get("imdbID");
+                        System.out.println(shows[i][0]);
+                        System.out.println(shows[i][1]);
+                    }
+                    return shows;
+                }
+
+            }
+            else {
+                return null;
+            }
+        } catch (IOException | InterruptedException | ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+
+        /*
         HttpGet request = createGet("https://api.thetvdb.com/search/series?name=" + sbSearchTerms);
         JSONObject joResponse = getJSONFromRequest(request);
         String error = (String) joResponse.get("Error");
         if (error == null) {
             JSONArray jaResponse = (JSONArray) joResponse.get("data");
 
+
             String[][] shows = new String[jaResponse.size()][2];
             for (int i = 0; i < jaResponse.size(); i++) {
                 shows[i][0] = (String) ((JSONObject) jaResponse.get(i)).get("seriesName");
                 shows[i][1] = Long.toString((Long) ((JSONObject) jaResponse.get(i)).get("id"));
+                System.out.println(shows[i][0]);
+                System.out.println(shows[i][1]);
             }
             return shows;
         } else {
             System.out.println(error);
             return null;
         }
+
+         */
+
+
     }
 
     public JSONObject searchTheTVDBShow(String id) {
-        HttpGet request = createGet("https://api.thetvdb.com/series/" + id);
-        JSONObject joResponse = (JSONObject) getJSONFromRequest(request).get("data");
+        String apiKey = "be9629f";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.omdbapi.com/?i="+id+"&apikey="+apiKey+"&format="+"json"))
+                .header("Accept", "application/json")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        JSONObject joResponse = null;
+        try {
+            java.net.http.HttpResponse response = java.net.http.HttpClient.newHttpClient().send(request, java.net.http.HttpResponse.BodyHandlers.ofString());
+            JSONParser parser = new JSONParser();
+            joResponse = (JSONObject) parser.parse(response.body().toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         return joResponse;
     }
@@ -268,9 +355,9 @@ public class DatabaseReader {
     public Show generateShow(String[] arShow) {
         System.out.println("DatabaseReader: Generating show \"" + arShow[0] + "\"...");
         JSONObject joShow = searchTheTVDBShow(arShow[1]);
-        Show show = new Show((String) joShow.get("seriesName"));
-        show.setDescription((String) joShow.get("overview"));
-        show.setTvdbId(Long.toString((Long) joShow.get("id")));
+        Show show = new Show((String) joShow.get("Title"));
+        show.setDescription((String) joShow.get("Plot"));
+        //show.setTvdbId(Long.toString((Long) joShow.get("id")));
         show.setImdbId((String) joShow.get("imdbId"));
 
         int page = 1;
