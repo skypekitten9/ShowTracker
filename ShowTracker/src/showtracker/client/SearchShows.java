b70fa5158b8a9c1d6ca207fb5753ebe28b24529f
th da <theresa_dannberg@hotmail.com>
2021-03-09 10:15:02 +0100

    Update SearchShows.java
    Removed color green in searchpane

--- ShowTracker/src/showtracker/client/SearchShows.java
+++ ShowTracker/src/showtracker/client/SearchShows.java
@@ -54,7 +54,6 @@ public class SearchShows extends JPanel {
     }
 
     private void drawSearchBarPanel() {
-        jpSearchBar.setBackground(Color.GREEN);
         jpSearchBar.setSize(350, 100);
         jpSearchBar.setLayout(new FlowLayout());
         tfSearchBar.setPreferredSize(new Dimension(200, 20));
